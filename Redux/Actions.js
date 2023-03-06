082d0a71363eaed307652bd614604e373b1f4cb0
SuhasKM <suhasvarun1@gmail.com>
2023-03-06 23:07:48 +0530

    Changed the tab navigation

--- Redux/Actions.js
+++ Redux/Actions.js
@@ -1,8 +1,15 @@
 export const SET_USER='SET_USER';
+export const SET_CONVERSATION_TAB_SCREEN='SET_CONVERSATION_TAB_SCREEN';
 
 export const setUser=user=>dispatch=>{
    dispatch({
     type:SET_USER,
     payload:user,
+   })
+};
+export const setConversationTabScreen=conversationScreen=>dispatch=>{
+   dispatch({
+    type:SET_CONVERSATION_TAB_SCREEN,
+    payload:conversationScreen,
    })
 };
